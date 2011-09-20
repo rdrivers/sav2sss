b94788ec2a8b1821e41f338b87fd70fd81689383
imm <imm@iain-mackay.org>
2011-09-20 16:53:28 +0000

    Initial commit

---
+++ savschema.py
@@ -0,0 +1,268 @@
+# Schema implementation for reading SPSS binary .sav files
+
+# Copyright (C) 2009  Computable Functions Limited, UK
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+import exceptions
+import savbinary
+
+from schema import *
+
+import psyco
+psyco.full ()
+
+class SavSchemaError (exceptions.Exception): pass
+		
+def invertMap (map):
+	result = {}
+	for (key, value) in map.items():
+		result[value] = key
+	return result
+	
+len1Codes = ["%d" % i for i in xrange(0, 10)]
+len2Codes = ["%02d" % i for i in xrange(0, 100)]
+								
+class SAVSchema (SchemaRepresentation):
+
+	def __init__ (self):
+		SchemaRepresentation.__init__ (self)
+		self.savDataset = None
+		
+	def load (self, savDataset):
+		#print "Loading SavSchema %s" % name
+		self.savDataset = savDataset
+		self.schema = Schema()
+		nonDummyCount = 0
+		nonDummyMap = {}
+		for index, savVariable in enumerate (savDataset.variables):
+			if savVariable.isDummy: continue
+			nonDummyMap [index] = nonDummyCount
+			nonDummyCount += 1
+			if savVariable.labelList is not None:
+				answerList = AnswerList (self.schema, savVariable.name)
+				labelList = savDataset.labelLists [savVariable.labelList]
+				codes = [key for key in labelList.labels.keys ()]
+				codes.sort ()
+				for code in codes:
+					Answer (answerList).makeNew\
+						(None, code, labelList.labels [code])
+			else:
+				answerList = None
+				
+			variable = Variable (self.schema, savVariable.name, answerList)
+			variable.translatable = False
+			if savVariable.label is None or len (savVariable.label) == 0:
+				variable.ttext = savVariable.longName
+				variable.qtext = ""
+			else:
+				variable.ttext = savVariable.label
+				variable.qtext = savVariable.longName
+			if savVariable.labelList is not None:
+				variable.type = 'single'
+				variable.length = answerList.maxCode
+			elif savVariable.type_ > 0:
+				variable.type = 'character'
+				if savVariable.extendedStringLength or savVariable.type_ == 255:
+					if savDataset.sensibleStringLengths:
+						variable.length = savVariable.sensibleLength
+					else:
+						variable.length = savVariable.extendedStringLength
+				else:
+					variable.length = savVariable.type_
+			elif savVariable.write_.format_type == savbinary.datetimeFormatCode:
+				variable.type = 'character'
+				variable.length = 24
+			else:
+				variable.type = 'quantity'
+				if savVariable.write_.format_type == savbinary.floatFormatCode:
+					variable.translatable = True
+				variable.dp = savVariable.write_.dp
+				width = min (24, savVariable.write_.width)
+				if variable.dp > 0: width = max (1, width - (variable.dp + 1))
+				if width > 1:
+					variable.min = min (savVariable.min, -lengthMaxCode (width-1))
+				else:
+					variable.min = min (savVariable.min, 0)
+				variable.max = max (savVariable.max, lengthMaxCode (width))
+				absMin = lengthMaxCode (codeLength (variable.min))
+				if variable.min < 0.0:
+					variable.min = -absMin
+				else:
+					variable.min = absMin
+				variable.max = lengthMaxCode (codeLength (variable.max))
+			
+			variable.count = 1
+			
+			# self.schema.weightVariableSequence = variable.index
+			
+class SAVVariableValue (VariableValue):
+
+	def __init__ (self, dataset, index):
+		VariableValue.__init__ (self, dataset, index)
+		
+	def _prepareValue (self):
+		self.variableNumber, self.value = self.dataset.record [self.index]
+			
+	def _extractValue (self):
+		self._prepareValue ()
+		return self.value
+			
+class SAVSingleVariableValue (SAVVariableValue):
+				
+	def _extractValue (self):
+		self._prepareValue()
+		if self.value is not None:
+			try:
+				return int (self.value)
+			except:
+				raise SavSchemaError, "Can't extract code value %s" %\
+					(self.reportValue (),)
+			
+class SAVQuantityVariableValue (SAVVariableValue):
+		
+	def _extractValue (self):
+		self._prepareValue()
+		if self.value is not None:
+			try:
+				return float (self.value)
+			except:
+				raise SavSchemaError, "Can't extract quantity value %s" %\
+					(self.reportValue (),)
+						
+class SAVDataset (Dataset):
+
+	def __init__ (self, schemaRepresentation, savDataset):
+		Dataset.__init__ (self, schemaRepresentation, savDataset, True)
+		self.savStream = None
+
+	def _assignVariableValue (self, index):
+		variable = self.schema.variableSequence[index]
+		if variable.type == 'character':
+			return SAVVariableValue (self, index)
+		elif variable.type == 'quantity':
+			return SAVQuantityVariableValue (self, index)
+		elif variable.type == 'single':
+			return SAVSingleVariableValue (self, index)
+		raise SavSchemaError, "Can't decode variable: %s" % variable.name
+
+	def read (self):
+		try:
+			self.record = self.savStream.next ()
+			Dataset.read (self)
+			return True
+		except StopIteration, e:
+			return False
+		
+	def reset2 (self):
+		Dataset.reset2 (self)
+		self.savStream = self.dataStore.getCaseStream ()
+		
+	def close (self):
+		pass
+
+	
+if __name__ == "__main__":
+	import sys
+	import os.path
+	import getopt
+	import sssxmlschema
+			
+	def logExceptionText (details=None):
+		import traceback
+		if details is None:
+			details = sys.exc_info()
+		(type, value, tb) = details
+		return """Exception:	 %s
+		Description: %s
+		Traceback:	 %s""" %\
+			(type, value, traceback.extract_tb (tb))
+		
+	def logException (details=None):
+		print >>sys.stderr, logExceptionText (details)
+	
+	print """
+savschema  Copyright (C) 2009  Computable Functions Limited
+
+    This program comes with ABSOLUTELY NO WARRANTY.
+    This is free software, and you are welcome to redistribute it
+    under the conditions of the GNU General Public License
+    (http://www.gnu.org/licenses/gpl.txt).
+"""
+	
+	if len(sys.argv) < 2:
+		print "--Usage: savschema [-s] [-f] [-oOutputEncoding] SAV-file-name"
+		sys.exit(0)
+		
+	sensibleStringLengths = True
+	full = False
+	outputEncoding = "iso-8859-1"
+	optlist, args = getopt.getopt(sys.argv[1:], 'vsfo:')
+	for (option, value) in optlist:
+		if option == '-s':
+			sensibleStringLengths = False
+		if option == '-f':
+			full = True
+		if option == '-o':
+			outputEncoding = value
+			
+	(root, savExt) = os.path.splitext (args [0])
+	print "..Converting %s to %s.xml and %s.asc" %\
+		(args [0], root, root)
+
+	if len(args) == 1:
+		try:
+			savData = savbinary.SAVDataset (args [0], sensibleStringLengths)
+		except exceptions.Exception, e:
+			print "Can't load SAV file (%s)" % e
+			logException ()
+		else:
+			try:
+				savSchema = SAVSchema ()
+				savSchema.load (savData)
+				print "..SAV file %s loaded, %d variable(s), %d answer list(s)" %\
+					(args[0],
+					 len(savSchema.schema.variableSequence),
+					 len(savSchema.schema.answerListMap))
+				if full: savData.printMetadata (True)
+				newSchema = sssxmlschema.SSSXMLSchema().convert (savSchema.schema, "xx://yy")
+				newSchema.allocate()
+				outputXMLFile = open (root + ".xml", 'w')
+				newSchema.save (outputXMLFile)
+				outputXMLFile.close ()
+				SSSDataset = sssxmlschema.SSSDataset (newSchema, root + '.asc', False, outputEncoding)
+				savDataset = SAVDataset (savSchema, savData)
+				savDataset.convert (SSSDataset)
+				print "..%d case(s) recovered from proprietary format" % savDataset.recordNumber
+				distributions = SSSDataset.getDistributions ()
+				if full:
+					for vv, distribution in distributions:
+						itemCount = len (distribution)
+						others = 0
+					 	print "Variable\t%s Distribution" % vv.variable.name
+						for value, total in distribution.items ():
+							if itemCount < 10 or total.unweightedTotal >  1:
+								print "Value\t%s\t%s" % (value, total.unweightedTotal)
+							else:
+								others += 1
+						if others > 0:
+							print "Unlisted singleton values:", others
+						
+				SSSDataset.close ()
+				
+			except exceptions.Exception, e:
+				print "Cannot prepare triple-S XML dataset (%s)" % e
+				logException ()
+	else:
+		print  "--Usage: savschema [-v] [-s] [-f] [-oOutputEncoding] SAV-file-name"
---
+++ schema.py
@@ -0,0 +1,486 @@
+# Generalised interface to questionnaire survey data and metadata
+
+# Copyright (C) 2005  Computable Functions Limited, UK
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+
+import exceptions
+
+__metaclass__ = type
+		
+interestingNumbers = (2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000,
+											10000, 20000, 50000, 100000, 200000, 500000, 1000000)
+
+class SchemaError (exceptions.Exception): pass
+
+def forceEncoding (text, encoding='ascii'):
+	if type (text) != unicode:
+		return str (text)
+	try:
+		return text.encode(encoding)
+	except UnicodeError:
+		lchars = []
+		for char in text:
+			try:
+				lchars.append(char.encode(encoding))
+			except UnicodeError, e:
+				lchars.append("&#%d;" % ord(char))				
+		return ''.join(lchars)
+	
+def codeLength (maxCode):
+	if maxCode <0: return codeLength (-maxCode) + 1
+	if maxCode < 10: return 1
+	if maxCode < 100: return 2
+	if maxCode < 1000: return 3
+	if maxCode < 10000: return 4
+	if maxCode < 100000: return 5
+	if maxCode < 1000000: return 6
+	if maxCode < 10000000: return 7
+	if maxCode < 100000000: return 8
+	if maxCode < 1000000000: return 9
+	if maxCode < 10000000000: return 10 
+	if maxCode < 100000000000: return 11 
+	if maxCode < 1000000000000: return 12 
+	if maxCode < 10000000000000: return 13 
+	if maxCode < 100000000000000: return 14 
+	if maxCode < 1000000000000000: return 15 
+	if maxCode < 10000000000000000: return 16
+	if maxCode < 100000000000000000: return 17
+	if maxCode < 1000000000000000000: return 18
+	if maxCode < 10000000000000000000: return 19
+	if maxCode < 100000000000000000000: return 20
+	if maxCode < 1000000000000000000000: return 21
+	if maxCode < 10000000000000000000000: return 22
+	if maxCode < 100000000000000000000000: return 23
+	if maxCode < 1000000000000000000000000: return 24
+	raise SchemaError, "More than 24 digits required for code %s" % maxCode
+
+def lengthMaxCode (length):
+	if length > 24 or length < 1:
+		raise SchemaError, "Code lengths must be in range 1..24: %s" % length
+	maxCode = (9, 99, 999, 9999, 99999, 999999, 9999999, 99999999, 999999999, 9999999999,
+					99999999999, 999999999999, 9999999999999, 99999999999999,
+					999999999999999, 9999999999999999, 99999999999999999,
+					999999999999999999, 9999999999999999999, 99999999999999999999,
+					999999999999999999999, 9999999999999999999999, 99999999999999999999999,
+					999999999999999999999999)[length-1]
+	return maxCode
+	
+class Schema:
+	def __init__ (self):
+		self.name = ""
+		self.title = ""
+		self.variableSequence = []
+		self.variableMap = {}
+		self.answerListMap = {}
+		self.weightVariableSequence = None
+		self.serialVariableSequence = None
+		
+	def findVariable (self, name):
+		for (index, variable) in enumerate(self.variableSequence):
+			if variable.name == name: return index
+
+class SchemaRepresentation:
+
+	# Initialise, either as empty or from an existing schema
+	def __init__ (self, schema=None):
+		pass
+	
+	# Load from an external file
+	def load (self, name, file):
+		return self
+		
+	# Save to an external file
+	def save (self, file):
+		assert False,\
+			"Base 'save' method of SchemaRepresentation should not be called"
+		
+	# Assign an external representation.
+	def allocate (self):
+		assert False,\
+			"Base 'allocate' method of SchemaRepresentation should not be called"
+		
+	# Create a schema in this representation based on an existing one
+	def convert (self, aSchema):
+		self.schema = aSchema
+		self.allocate()
+		return self
+
+class VariableValue:
+
+	def __init__ (self, dataset, index):
+		self.dataset = dataset
+		self.variable = dataset.schema.variableSequence[index]
+		self.index = index
+		self.enabled = False
+		self.value = None
+		
+	def getType (self):
+		return self.variable.type
+		
+	def _enable (self):
+		self.enabled = True
+		filter = self.variable.baseVariableIndex
+		if filter is not None:
+			self.dataset.variableValueSequence[filter]._enable()
+					
+	def _disable (self):
+		self.enabled = False
+		
+	def _requireEnabled (self):
+		if not self.enabled:
+			raise SchemaError, "Access to disabled variable: %s" % self.variable.name
+			
+	def _checkBase (self):
+		filter = self.variable.baseVariableIndex
+		if filter is not None:
+			return self.dataset.variableValueSequence[filter]._getValue() == self.variable.baseVariableFilterValue
+		return True
+				
+	def _extractValue (self):
+		pass		
+
+	def _insertValue (self):
+		pass
+		
+	def getValue (self):
+		self._requireEnabled()
+		if self._checkBase (): return self._getValue()
+		
+	def _getValue (self):
+		return self.value
+			
+	def setValue (self, value):
+		self._requireEnabled()
+		self._setValue(value)
+		
+	def _setValue (self, value):
+		self.value = value
+		
+	def reportValue (self):
+		return "Variable %s at record %s (%s)" %\
+			(self.variable.name, self.dataset.recordNumber,
+			 self.variable.displayValue(self.value))
+
+class Dataset:
+
+	# Initialise, specifying a schema representation
+	
+	def __init__ (self, schemaRepresentation, dataStore, isInput=True):
+		self.schemaRepresentation = schemaRepresentation
+		self.schema = schemaRepresentation.schema
+		self.dataStore = dataStore
+		self.isInput = isInput
+		self.variableValueSequence =\
+			[self._assignVariableValue (index) for index in xrange(0,len(self.schema.variableSequence))]
+		self.reset1 ()
+		
+	# assign a VariableValue subclass instance appropriate to the dataset type and variable type
+	def _assignVariableValue (self, index):
+		return VariableValue (self, index)
+			
+	# Find the variable value instance corresponding to a particular variable name
+	def getVariableValue (self, name):
+		return self.variableValueSequence[self.schema.variableMap[name].index]
+		
+	# Prepare to make a pass over the dataset
+	def reset1 (self):
+		self.recordNumber = None
+		for variableValue in self.variableValueSequence:
+			variableValue._disable()
+	
+	# Make a variable value available during the next pass
+	def enable (self, variableValue):
+		variableValue._enable()
+		
+	# Begin pass making new value of each enabled variable available per advance
+	def reset2 (self):
+		self.enabledValues = []
+		for variableValue in self.variableValueSequence:
+			if variableValue is not None and variableValue.enabled:
+				self.enabledValues.append (variableValue)
+		self.recordNumber = 0
+		
+	# Return True if new record available from the data store, False if not
+	def read (self):
+		self.recordNumber += 1
+		for variableValue in self.enabledValues:
+			variableValue._extractValue()
+		return True
+		
+	# Write the record to the data store
+	def write (self):
+		self.recordNumber += 1
+		for variableValue in self.enabledValues:
+			variableValue._insertValue ()
+		
+	def recordCount (self):
+		self.reset1 ()
+		self.reset2 ()
+		recordsRead = 0
+		moreData = self.read ()
+		while moreData:
+			recordsRead += 1
+			moreData = self.read ()
+		return recordsRead
+		
+	def close (self):
+		pass
+		
+	def getDistributions (self):
+				self.reset1 ()
+				weightSequence = self.schema.weightVariableSequence
+				if weightSequence is not None:
+					weightvv = self.getVariableValue (self.schema.variableSequence[weightSequence])
+					self.enable (weightvv)
+				distributions = []
+				for variable in self.schema.variableSequence:
+					vv = self.getVariableValue (variable.name)
+					self.enable (vv)
+					distributions.append ((vv, {}))
+				self.reset2 ()
+				moreData = self.read()
+				while moreData:
+					for (vv, distribution) in distributions:
+						if weightSequence is not None:
+							weight = vv.getValue ()
+						else:
+							weight = 1.0
+						value = vv.getValue()
+						if type (value) == list:
+							for individualValue in value:
+								try:
+									total = distribution[individualValue]
+								except:
+									total = Total()
+									distribution[individualValue] = Total()
+								total.increment(weight)
+						else:
+							try:
+								total = distribution [value]
+							except:
+								total = Total()
+								distribution [value] = total
+							total.increment(weight)
+					moreData = self.read()
+				return distributions
+					
+	def convert (self, outputDataset, progressReporter = None):
+		conversionSequence = []
+		for variable in self.schema.variableSequence:
+			vv = self.getVariableValue (variable.name)
+			try:
+				ovv = outputDataset.getVariableValue (variable.name)
+				outputDataset.enable(ovv)
+				self.enable (vv)
+				conversionSequence.append ((vv, ovv))
+			except Exception, e:
+				raise SchemaError, "Can't enable variable %s in output dataset" % variable.name
+		self.reset2()
+		outputDataset.reset2()
+		moreData = self.read()
+		recordCount = 0
+		while moreData:
+			recordCount += 1
+			for (vv, ovv) in conversionSequence:
+				ovv._setValue (vv._getValue())
+			outputDataset.write()
+			if progressReporter is not None:
+				if recordCount in interestingNumbers:
+					progressReporter (recordCount, 
+						"%d record(s) processed" % recordCount)
+				else:		
+					progressReporter (recordCount, None)
+			moreData = self.read()
+
+class Total:
+
+	def __init__ (self):
+		self.unweightedTotal = 0
+		self.weightedTotal = 0.0
+		
+	def increment (self, weight=1.0):
+		self.unweightedTotal += 1
+		self.weightedTotal += weight
+		
+	def __str__ (self):
+		return "%s\t%s"  % (self.unweightedTotal, self.weightedTotal)
+		
+class Variable:
+	def __init__ (self, schema, name, answerList = None):
+		self.name = name
+		self.schema = schema
+		self.answerList = answerList
+		if answerList	is not None:
+			self.answerList.useCount += 1 
+		self.index = len(schema.variableSequence)
+		schema.variableSequence.append (self)
+		schema.variableMap[name] = self
+		self.qtext = None
+		self.baseVariableIndex = None
+		# subclasses add attributes:
+		# index: index in record
+		# type: single/multiple/quantity/logical/character
+		# ttext: Title for the variable
+		# qtext: Associated question (if any)
+		# length: Number of categories (categorical)
+		#				 Number of characters (character string)
+		# count: max number of values (1 unless multiple)
+		# dp:		number of decimal places (if quantity)
+		# min:	 minimum value (quantity)
+		# max:	 maximum value (quantity)
+		
+	def displayValue (self, value):
+		if value is None: return ""
+		if self.type in ("single", "multiple"):
+			return self.answerList.answerText (value)
+		elif self.type == "quantity":
+			if self.dp > 0: return "%0.*f" % (self.dp, value)
+			else: return str (value)
+		elif self.type== "logical":
+			if value: return "Yes"
+			else:		 return "No"
+		else:
+			return self.value		
+	
+	def displayValueRange (self, min, max):
+		if self.type == 'logical':
+			return self.ttext
+		if min is None and max is None: return ""
+		if min == max:
+			return "%s: %s" % (self.ttext, self.displayValue (min))
+		else:
+			return "%s: %s...%s" % (self.ttext,
+				self.displayValue (min),
+				self.displayValue (max))
+				
+	def compareAnswers (self, other):
+		if self.type in ("single", "multiple"):
+			if other.type not in ("single", "multiple"):
+				return False
+			if self.length != other.length:
+				return False
+			otherAnswers = other.answerList.answers ()
+			for answer in self.answerList.answers ():
+				otherAnswer = otherAnswers. next ()
+				if answer.code != otherAnswer.code or\
+					 answer.name != otherAnswer.name or\
+					 answer.text != otherAnswer.text:
+					return False
+			return True
+		elif self.type != other.type:
+			return False
+		elif self.type == "logical":
+			return True
+		elif self.type == "quantity":
+			return (self.dp == other.dp) and\
+				(self.min == other.min) and\
+				(self.max == other.max)
+		elif self.type == "character":
+			return self.length == other.length
+		
+class AnswerList:
+	def __init__ (self, schema, name):
+		self.name = name
+		self.schema = schema
+		if name is not None:
+			schema.answerListMap[name] = self
+		self.answerSequence = []
+		self.answerMap = {}
+		self.useCount = 0
+		self.maxCode = None
+		self.minCode = None
+		self.simple = True
+		
+	def answers(self):
+		nameMap = self.schema.answerListMap
+		for item in self.answerSequence:
+			if not item.isUse:
+				yield item
+			else:
+				list = nameMap[item.name]
+				for item in list.answers():
+					yield item
+					
+	def findAnswerWithText (self, text):
+		for answer in self.answers():
+			if answer.text == text: return answer
+					
+	def findAnswerWithCode (self, code):
+		for answer in self.answers():
+			if answer.code == code: return answer
+					
+	def isSimpleList (self):
+		return self.simple
+		#currentCode = 0
+		#for answer in self.answerSequence:
+		#	currentCode += 1
+		#	if answer.isUse or currentCode != answer.code:
+		#		return False
+		#return True
+		
+	def answerText (self, code):
+		if self.simple:
+			return self.answerSequence [code-1].text
+		else:
+			return "#%s" % code
+				
+class Answer:
+	def __init__ (self, answerList):
+		self.answerList = answerList
+		answerList.answerSequence.append (self)
+		
+	def makeNew (self, name, code, text):
+		self.isUse = False
+		self.text = text
+		self.name = name
+		if code is None:
+			if self.answerList.maxCode:
+				proposedCode = self.answerList.maxCode + 1
+			else:
+				proposedCode = 1
+		else:
+			proposedCode = code
+		if self.answerList.answerMap.has_key (code):
+			raise SchemaError, "Duplicated code %s in answer list %s" %\
+				(proposedCode, self.answerList.name)
+		self.code = proposedCode
+		self.answerList.answerMap [proposedCode] = self
+		if self.answerList.maxCode:
+			self.answerList.maxCode = max(self.answerList.maxCode, proposedCode)
+		else:
+			self.answerList.maxCode = proposedCode
+		if self.answerList.minCode:
+			self.answerList.minCode = min(self.answerList.minCode, proposedCode)
+		else:
+			self.answerList.minCode = proposedCode
+		self.answerList.simple = self.answerList.simple and\
+			proposedCode == len(self.answerList.answerSequence)
+		
+	def makeUse (self, name):
+		self.isUse = True
+		self.name = name
+		usedList = self.answerList.schema.answerListMap[name]
+		for usedAnswer in usedList.answers():
+			self.answerList.answerMap [usedAnswer.code] = usedAnswer
+		usedList.useCount += 1
+		if self.answerList.maxCode is None:
+			self.answerList.maxCode = usedList.maxCode
+		else:
+			self.answerList.maxCode = max(self.answerList.maxCode, usedList.maxCode)
+		if self.answerList.minCode is None:
+			self.answerList.minCode = usedList.minCode
+		else:
+			self.answerList.minCode = min(self.answerList.minCode, usedList.minCode)
